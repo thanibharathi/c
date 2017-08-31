625224ffd429638438f3f56d8ad6d0f8312ada12
thanibharathi <31512670+thanibharathi@users.noreply.github.com>
2017-08-31 20:24:06 +0530

    Number.c

---
+++ Number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+Void main()
+{
+int i,j;
+scanf("%d",i);
+i=j;
+if(i==0)
+{
+  printf("Zero");
+  }
+elseif(i==j)
+{
+  printf("positive");
+  }
+else
+{
+  printf("negative");
+  }
+}

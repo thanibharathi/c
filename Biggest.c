04442bb92d3c5e31e6a44dd08a10188d760561f8
thanibharathi <31512670+thanibharathi@users.noreply.github.com>
2017-08-31 20:46:22 +0530

    Biggest.c

---
+++ Biggest.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+Void main()
+{
+int i,j,k;
+scanf("%d%d%d",i,j,k);
+if((i>j)&&(i>k))
+{
+  printf("first number is biggest");
+  }
+elseif(j>k)
+{
+  printf("second number is biggest");
+  }
+else
+{
+  printf("third number is biggest ");
+  }
+}

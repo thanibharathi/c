53e10cd5f42b2a32368c814f80b10f9f18e4d7fb
thanibharathi <31512670+thanibharathi@users.noreply.github.com>
2017-09-01 16:08:48 +0530

    Prime.c

---
+++ Prime.c
@@ -0,0 +1,19 @@
+include<stdio.h>
+#include<conio.h>
+Void main()
+{
+int i,j=0,k;
+scanf("%d",i);
+for(k=0;k<=i;k++)
+{
+if(i/i==0)
+{
+  j=j+1;
+  }
+else
+{
+  j=j;
+  }
+  printf("%d",j);
+  }
+  }
